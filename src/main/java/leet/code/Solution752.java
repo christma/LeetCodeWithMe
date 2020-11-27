8e82481d60d13ee539dcf9c68cefcc1e6fa85a34
Christmas <46958618+christma@users.noreply.github.com>
2020-11-27 17:18:00 +0800

    Add files via upload
    Solution752

---
+++ src/main/java/leet/code/Solution752.java
@@ -0,0 +1,77 @@
+package com.spring.leetCode;
+
+import java.util.*;
+
+public class Solution752 {
+    // 将 s[j] 向上拨动一次
+    String plusOne(String s, int j) {
+        char[] ch = s.toCharArray();
+        if (ch[j] == '9') {
+            ch[j] = '0';
+        } else {
+            ch[j] += 1;
+        }
+        return new String(ch);
+    }
+
+    // 将 s[i] 向下拨动一次
+    String minusOne(String s, int j) {
+        char[] ch = s.toCharArray();
+        if (ch[j] == '0') {
+            ch[j] = '9';
+        } else {
+            ch[j] -= 1;
+        }
+        return new String(ch);
+    }
+
+    // BFS 框架，打印出所有可能的密码
+    int BFS(String[] deadends, String target) {
+        Set<String> deads = new HashSet<>();
+        Collections.addAll(deads, deadends);
+        Set<String> visited = new HashSet<>();
+        visited.add("0000");
+        Queue<String> q = new LinkedList<>();
+        q.offer("0000");
+
+        int step = 0;
+        while (!q.isEmpty()) {
+            int sz = q.size();
+            /* 将当前队列中的所有节点向周围扩散 */
+            for (int i = 0; i < sz; i++) {
+                String cur = q.poll();
+                /* 判断是否到达终点 */
+                if (deads.contains(cur)) {
+                    continue;
+                }
+                if (cur.equals(target)) {
+                    return step;
+                }
+
+                /* 将一个节点的相邻节点加入队列 */
+                for (int j = 0; j < 4; j++) {
+                    String up = plusOne(cur, j);
+                    if (!visited.contains(up)) {
+                        q.offer(up);
+                        visited.add(up);
+                    }
+                    String down = minusOne(cur, j);
+                    if (!visited.contains(down)) {
+                        q.offer(down);
+                        visited.add(down);
+                    }
+                }
+
+            }
+            /* 在这里增加步数 */
+            step++;
+        }
+        return -1;
+    }
+    
+    public static void main(String[] args) {
+        String[] str = {"0201", "0101", "0102", "1212", "2002"};
+
+        System.out.println(new Solution752().BFS(str, "0202"));
+    }
+}
