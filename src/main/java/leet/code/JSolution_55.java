de3973a89a0cda4fc4bbf26d2ed902ee78889d6a
Christmas <46958618+christma@users.noreply.github.com>
2020-12-30 10:22:57 +0800

    JSolution_55

---
+++ src/main/java/leet/code/JSolution_55.java
@@ -0,0 +1,21 @@
+package com.spring.leetCode;
+
+public class JSolution_55 {
+    public class TreeNode {
+        int val;
+        TreeNode left;
+        TreeNode right;
+
+        TreeNode(int x) {
+            val = x;
+        }
+    }
+
+    public int maxDepth(TreeNode root) {
+        if (root == null) {
+            return 0;
+        }
+        return Math.max(maxDepth(root.left), maxDepth(root.right)) + 1;
+    }
+
+}
