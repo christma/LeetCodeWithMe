ecc0a5afbcc8a82198abf26fb7204d3e18bffe06
Christmas <46958618+christma@users.noreply.github.com>
2020-12-31 15:01:55 +0800

    Solution534

---
+++ src/main/java/leet/code/Solution534.java
@@ -0,0 +1,31 @@
+package com.spring.leetCode;
+
+public class Solution534 {
+    public class TreeNode {
+        int val;
+        TreeNode left;
+        TreeNode right;
+
+        TreeNode(int x) {
+            val = x;
+        }
+    }
+
+    int ans = 0;
+
+    public int diameterOfBinaryTree(TreeNode root) {
+
+        dfs(root);
+        return ans;
+    }
+
+    private int dfs(TreeNode node) {
+        if (node == null) {
+            return 0;
+        }
+        int L = dfs(node.left);
+        int R = dfs(node.right);
+        ans = Math.max(ans, L + R);
+        return Math.max(R, L) + 1;
+    }
+}
