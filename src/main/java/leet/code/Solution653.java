5df4954d2b570410d92d684800bb6d3a6c6bd3ce
Christmas <46958618+christma@users.noreply.github.com>
2020-12-31 14:29:01 +0800

    Solution653

---
+++ src/main/java/leet/code/Solution653.java
@@ -0,0 +1,45 @@
+package com.spring.leetCode;
+
+import java.util.HashSet;
+import java.util.Set;
+
+public class Solution653 {
+
+
+    public class TreeNode {
+        int val;
+        TreeNode left;
+        TreeNode right;
+
+        TreeNode() {
+        }
+
+        TreeNode(int val) {
+            this.val = val;
+        }
+
+        TreeNode(int val, TreeNode left, TreeNode right) {
+            this.val = val;
+            this.left = left;
+            this.right = right;
+        }
+    }
+
+    public boolean findTarget(TreeNode root, int k) {
+        Set<Integer> set = new HashSet<>();
+        return find(root, k, set);
+    }
+
+    private boolean find(TreeNode node, int k, Set<Integer> set) {
+        if (node == null) {
+            return false;
+        }
+
+        if (set.contains(k - node.val)) {
+            return true;
+        }
+        set.add(node.val);
+        return find(node.left, k, set) || find(node.right, k, set);
+    }
+
+}
