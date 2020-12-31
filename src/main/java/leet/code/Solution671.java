a8e2c24635d0091e60b07fb96e98de1bd9298d6e
Christmas <46958618+christma@users.noreply.github.com>
2020-12-31 17:36:35 +0800

    Solution671

---
+++ src/main/java/leet/code/Solution671.java
@@ -0,0 +1,47 @@
+package com.spring.leetCode;
+
+import java.util.*;
+
+public class Solution671 {
+    public class TreeNode {
+        int val;
+        TreeNode left;
+        TreeNode right;
+
+        TreeNode() {
+        }
+
+        TreeNode(int val) {
+            this.val = val;
+        }
+
+        TreeNode(int val, TreeNode left, TreeNode right) {
+            this.val = val;
+            this.left = left;
+            this.right = right;
+        }
+    }
+
+    public int findSecondMinimumValue(TreeNode root) {
+        Set<Integer> set = new HashSet<>();
+        List<Integer> list = new ArrayList<>();
+        Queue<TreeNode> queue = new LinkedList<>();
+        queue.add(root);
+        while (!queue.isEmpty()) {
+            TreeNode node = queue.remove();
+            set.add(node.val);
+            if(node.left != null){
+                queue.add(node.left);
+            }
+            if(node.right != null){
+                queue.add(node.right);
+            }
+        }
+        list.addAll(set);
+        Collections.sort(list);
+        if(list.size() < 2){
+            return -1;
+        }
+        return  list.get(1);
+    }
+}
