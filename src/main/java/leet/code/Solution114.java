b677daaefb8bc5187ea9fe0f1d0fc85b7009e445
Christmas <46958618+christma@users.noreply.github.com>
2021-01-15 11:21:27 +0800

    Solution114

---
+++ src/main/java/leet/code/Solution114.java
@@ -0,0 +1,39 @@
+package com.spring.leetCode;
+
+public class Solution114 {
+    public class TreeNode {
+        int val;
+        TreeNode left;
+        TreeNode right;
+
+        TreeNode() {
+        }
+
+        TreeNode(int val) {
+            this.val = val;
+        }
+
+        TreeNode(int val, TreeNode left, TreeNode right) {
+            this.val = val;
+            this.left = left;
+            this.right = right;
+        }
+    }
+
+    public void flatten(TreeNode root) {
+        while (root != null) {
+            if (root.left == null) {
+                root = root.right;
+            } else {
+                TreeNode pre = root.left;
+                while (pre.right != null) {
+                    pre = pre.right;
+                }
+                pre.right = root.right;
+                root.right = root.left;
+                root.left = null;
+                root = root.right;
+            }
+        }
+    }
+}
