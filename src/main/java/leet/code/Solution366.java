f04c1e071f549e3635da2bddb9126018d0c191d9
christma <liuchuanmumumu@gmail.com>
2021-03-15 22:24:42 +0800

    fix bug

--- src/main/java/leet/code/Solution366.java
+++ src/main/java/leet/code/Solution366.java
@@ -4,7 +4,7 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class Solution366 {
-    public class TreeNode {
+    public static class TreeNode {
         int val;
         TreeNode left;
         TreeNode right;
@@ -29,7 +29,7 @@ public class Solution366 {
         while (root != null) {
             List<Integer> list = new ArrayList<>();
 
-            helper(root, list);
+            root = helper(root, list);
             resList.add(list);
         }
         return resList;
@@ -46,5 +46,20 @@ public class Solution366 {
         root.left = helper(root.left, list);
         root.right = helper(root.right, list);
         return root;
+    }
+
+    public static void main(String[] args) {
+        TreeNode t1 = new TreeNode(1);
+        TreeNode t2 = new TreeNode(2);
+        TreeNode t3 = new TreeNode(3);
+        TreeNode t4 = new TreeNode(4);
+        TreeNode t5 = new TreeNode(5);
+
+        t1.left = t2;
+        t1.right = t3;
+        t2.left = t4;
+        t2.right = t5;
+
+        new Solution366().findLeaves(t1);
     }
 }
