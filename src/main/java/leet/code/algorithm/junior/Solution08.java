ee33a76c068b2bda32e3576651950ea46e6dc8b1
christma <liuchuanmumumu@gmail.com>
2021-03-18 21:48:01 +0800

    Solution08

---
+++ src/main/java/leet/code/algorithm/junior/Solution08.java
@@ -0,0 +1,22 @@
+package leet.code.algorithm.junior;
+
+import java.util.HashMap;
+
+public class Solution08 {
+    public int[] twoSum(int[] nums, int target) {
+
+        int[] result = new int[2];
+        HashMap<Integer, Integer> hashMap = new HashMap<>();
+        for (int i = 0; i < nums.length; i++) {
+            if (hashMap.containsKey(target - nums[i])) {
+                result[0] = i;
+                result[1] = hashMap.get(target - nums[i]);
+                break;
+            } else {
+                hashMap.put(nums[i], i);
+            }
+        }
+        return result;
+
+    }
+}
