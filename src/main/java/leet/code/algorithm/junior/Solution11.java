f2a7d13291c5e453e55261cbcc421cc285c93580
christma <liuchuanmumumu@gmail.com>
2021-03-20 21:28:14 +0800

    Solution11

---
+++ src/main/java/leet/code/algorithm/junior/Solution11.java
@@ -0,0 +1,22 @@
+package leet.code.algorithm.junior;
+
+public class Solution11 {
+
+    public void rotate(int[][] matrix) {
+
+        int length = matrix.length;
+        for (int i = 0; i < length / 2; i++) {
+            for (int j = i; j < length - i - 1; j++) {
+
+                int temp = matrix[i][j];
+                int m = length - j - 1;
+                int n = length - i - 1;
+                matrix[i][j] = matrix[m][i];
+                matrix[m][i] = matrix[n][m];
+                matrix[n][m] = matrix[j][n];
+                matrix[j][n] = temp;
+            }
+        }
+    }
+
+}
