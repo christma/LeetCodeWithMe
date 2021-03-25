7ee5587626753bad3f5befde7dfdc5740078fb86
Christmas <46958618+christma@users.noreply.github.com>
2021-03-25 17:38:10 +0800

    tree

---
+++ src/main/java/leet/code/algorithm/junior/tree/Solution04.java
@@ -0,0 +1,60 @@
+package algorithm.junior.tree;
+
+import org.apache.spark.sql.sources.In;
+
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Queue;
+
+public class Solution04 {
+    static class TreeNode {
+        int val;
+        TreeNode left;
+        TreeNode right;
+
+        TreeNode() {
+        }
+
+        TreeNode(int val) {
+            this.val = val;
+        }
+
+        TreeNode(int val, TreeNode left, TreeNode right) {
+            this.val = val;
+            this.left = left;
+            this.right = right;
+        }
+    }
+
+    public List<List<Integer>> levelOrder(TreeNode root) {
+
+        List<List<Integer>> res = new LinkedList<>();
+        Queue<TreeNode> queue = new LinkedList<>();
+        if(root == null){
+            return res;
+        }
+        queue.offer(root);
+        while (!queue.isEmpty()) {
+            int levelNum = queue.size();
+            List<Integer> subList = new ArrayList<>();
+            for (int i = 0; i < levelNum; i++) {
+                TreeNode node = queue.poll();
+                subList.add(node.val);
+                if (node.left != null) {
+                    queue.offer(node.left);
+                }
+                if (node.right != null) {
+                    queue.offer(node.right);
+                }
+            }
+            res.add(subList);
+
+        }
+        return res;
+    }
+
+    public static void main(String[] args) {
+
+    }
+}
---
+++ src/main/java/leet/code/algorithm/junior/tree/Solution05.java
@@ -0,0 +1,40 @@
+package algorithm.junior.tree;
+
+public class Solution05 {
+    static class TreeNode {
+        int val;
+        TreeNode left;
+        TreeNode right;
+
+        TreeNode() {
+        }
+
+        TreeNode(int val) {
+            this.val = val;
+        }
+
+        TreeNode(int val, TreeNode left, TreeNode right) {
+            this.val = val;
+            this.left = left;
+            this.right = right;
+        }
+    }
+
+    public TreeNode sortedArrayToBST(int[] num) {
+        if (num.length == 0) {
+            return null;
+        }
+        return sortedArrayToBST(num, 0, num.length - 1);
+    }
+
+    private TreeNode sortedArrayToBST(int[] num, int start, int end) {
+        if (start > end) {
+            return null;
+        }
+        int mid = start + (end - start) / 2;
+        TreeNode root = new TreeNode(num[mid]);
+        root.left = sortedArrayToBST(num, start, mid - 1);
+        root.right = sortedArrayToBST(num, mid + 1, end);
+        return root;
+    }
+}
