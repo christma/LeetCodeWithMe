2b5c8dbf7340e04d2889f2e41544f039563cddfc
christma <liuchuanmumumu@gmail.com>
2021-03-28 22:08:17 +0800

    dynamic

--- src/main/java/leet/code/algorithm/junior/dynamic/Solution04.java
+++ src/main/java/leet/code/algorithm/junior/dynamic/Solution04.java
@@ -14,7 +14,12 @@ public class Solution04 {
             dp[i][0] = Math.max(dp[i - 1][0], dp[i - 1][1]);
             dp[i][1] = dp[i - 1][0] + nums[i];
         }
+
         return Math.max(dp[length - 1][0], dp[length - 1][1]);
+
+    }
+
+    public static void main(String[] args) {
 
     }
 }
