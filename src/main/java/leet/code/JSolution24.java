d1716d908e7207400b46668c86b180985d847537
Christmas <46958618+christma@users.noreply.github.com>
2021-03-31 17:21:35 +0800

    JSolution24

---
+++ src/main/java/leet/code/JSolution24.java
@@ -0,0 +1,30 @@
+package com.spring.leetCode;
+
+public class JSolution24 {
+    public ListNode reverseList(ListNode head) {
+        ListNode prev = null;
+        ListNode curr = head;
+        while (curr != null) {
+            ListNode next = curr.next;
+            curr.next = prev;
+            prev = curr;
+            curr = next;
+        }
+        return prev;
+    }
+
+    public static void main(String[] args) {
+        ListNode ls1 = new ListNode(1);
+        ListNode ls2 = new ListNode(2);
+        ListNode ls3 = new ListNode(3);
+        ListNode ls4 = new ListNode(4);
+        ListNode ls5 = new ListNode(5);
+
+        ls1.next = ls2;
+        ls2.next = ls3;
+        ls3.next = ls4;
+        ls4.next = ls5;
+
+        new JSolution24().reverseList(ls1);
+    }
+}
