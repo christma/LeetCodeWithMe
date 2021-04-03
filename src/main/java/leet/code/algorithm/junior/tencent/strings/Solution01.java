b24054a7e2ab842d753adbe65401be9f6a891f71
christma <liuchuanmumumu@gmail.com>
2021-04-03 23:05:12 +0800

    Solution01

---
+++ src/main/java/leet/code/algorithm/junior/tencent/strings/Solution01.java
@@ -0,0 +1,17 @@
+package leet.code.algorithm.junior.tencent.strings;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class Solution01 {
+    public int[] twoSum(int[] nums, int target) {
+        Map<Integer, Integer> map = new HashMap<>();
+        for (int i = 0; i < nums.length; i++) {
+            if (map.get(target - nums[i]) != null) {
+                return new int[]{map.get(target - nums[i]), i};
+            }
+            map.put(nums[i], i);
+        }
+        return new int[]{0};
+    }
+}
