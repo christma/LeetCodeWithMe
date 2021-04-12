4cdeeee66812d231df761ca100dea0cccd5087d5
Christmas <46958618+christma@users.noreply.github.com>
2021-04-12 10:48:21 +0800

    Add files via upload

---
+++ src/main/java/leet/code/algorithm/junior/tencent/linkedlist/Solution01.java
@@ -0,0 +1,36 @@
+package tencent.linkedlist;
+
+public class Solution01 {
+
+    static class ListNode {
+
+        int val;
+
+        ListNode next;
+
+        ListNode() {
+        }
+
+        ListNode(int val) {
+            this.val = val;
+        }
+
+        ListNode(int val, ListNode next) {
+            this.val = val;
+            this.next = next;
+        }
+
+    }
+
+    public ListNode reverseList(ListNode head) {
+        ListNode prev = null;
+        ListNode cur = head;
+        while (cur != null) {
+            ListNode next = cur.next;
+            cur.next = prev;
+            prev = cur;
+            cur = next;
+        }
+        return prev;
+    }
+}
---
+++ src/main/java/leet/code/algorithm/junior/tencent/linkedlist/Solution03.java
@@ -0,0 +1,43 @@
+package tencent.linkedlist;
+
+import com.google.inject.internal.cglib.core.$Customizer;
+
+public class Solution03 {
+    static class ListNode {
+
+        int val;
+
+        ListNode next;
+
+        ListNode() {
+        }
+
+        ListNode(int val) {
+            this.val = val;
+        }
+
+        ListNode(int val, ListNode next) {
+            this.val = val;
+            this.next = next;
+        }
+
+    }
+
+    public ListNode mergeTwoLists(ListNode l1, ListNode l2) {
+
+        ListNode head = new ListNode(0), tail = head;
+        while (l1 != null && l2 != null) {
+            if (l1.val < l2.val) {
+                tail.next = l1;
+                l1 = l1.next;
+            } else {
+                tail.next = l2;
+                l2 = l2.next;
+            }
+            tail = tail.next;
+        }
+        tail.next = l1 != null ? l1 : l2;
+        return head.next;
+    }
+
+}
