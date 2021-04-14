b783ed4e1a719cf1f6c8dd1e48478afffa849ecb
christma <liuchuanmumumu@gmail.com>
2021-04-14 22:29:43 +0800

    Solution05

---
+++ src/main/java/leet/code/algorithm/junior/tencent/linkedlist/Solution05.java
@@ -0,0 +1,47 @@
+package leet.code.algorithm.junior.tencent.linkedlist;
+
+public class Solution05 {
+    static class ListNode {
+        int val;
+        ListNode next;
+
+        ListNode() {
+        }
+
+        ListNode(int val) {
+            this.val = val;
+        }
+
+        ListNode(int val, ListNode next) {
+            this.val = val;
+            this.next = next;
+        }
+    }
+
+    public ListNode rotateRight(ListNode head, int k) {
+        if (k == 0 || head == null || head.next == null) {
+            return head;
+        }
+        int n = 1;
+        ListNode iter = head;
+        while (iter.next != null) {
+            n++;
+            iter = iter.next;
+        }
+        int add = n - k % n;
+        if (add == n) {
+            return head;
+        }
+        iter.next = head;
+        while (add-- > 0) {
+            iter = iter.next;
+        }
+        ListNode ret = iter.next;
+        iter.next = null;
+        return ret;
+    }
+
+    public static void main(String[] args) {
+        System.out.println( 4 % 6);
+    }
+}
