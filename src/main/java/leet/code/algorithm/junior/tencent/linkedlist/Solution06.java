7cbd1125abd7073ca87a7200d3ab9d94c2014225
christma <liuchuanmumumu@gmail.com>
2021-04-16 22:14:10 +0800

    Solution06

---
+++ src/main/java/leet/code/algorithm/junior/tencent/linkedlist/Solution06.java
@@ -0,0 +1,44 @@
+package leet.code.algorithm.junior.tencent.linkedlist;
+
+import java.util.HashSet;
+import java.util.Set;
+
+public class Solution06 {
+
+    class ListNode {
+        int val;
+        ListNode next;
+
+        ListNode(int x) {
+            val = x;
+            next = null;
+        }
+    }
+
+    public boolean hasCycle(ListNode head) {
+        Set<ListNode> set = new HashSet<>();
+        while (head != null) {
+            if (!set.add(head)) {
+                return true;
+            }
+            head = head.next;
+        }
+        return false;
+    }
+
+    public boolean hasCycle1(ListNode head) {
+        if (head == null || head.next == null) {
+            return false;
+        }
+        ListNode slow = head;
+        ListNode fast = head.next;
+        while (slow != fast) {
+            if (fast == null || fast.next == null) {
+                return false;
+            }
+            slow = slow.next;
+            fast = fast.next.next;
+        }
+        return true;
+    }
+}
