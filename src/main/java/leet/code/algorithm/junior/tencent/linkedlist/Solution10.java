1097e7dae9a94f8e9170bfc253b75b6756e189ce
christma <liuchuanmumumu@gmail.com>
2021-04-19 21:33:25 +0800

    Solution10

---
+++ src/main/java/leet/code/algorithm/junior/tencent/linkedlist/Solution10.java
@@ -0,0 +1,17 @@
+package leet.code.algorithm.junior.tencent.linkedlist;
+
+public class Solution10 {
+    static class ListNode {
+        int val;
+        ListNode next;
+
+        ListNode(int x) {
+            val = x;
+        }
+    }
+
+    public void deleteNode(ListNode node) {
+        node.val = node.next.val;
+        node.next = node.next.next;
+    }
+}
