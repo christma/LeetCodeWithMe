0620f1b832be67c3fdac8f4315020f525196d449
christma <liuchuanmumumu@gmail.com>
2021-04-21 22:44:11 +0800

    Solution02

---
+++ src/main/java/leet/code/algorithm/junior/tencent/sortandfind/Solution02.java
@@ -0,0 +1,27 @@
+package leet.code.algorithm.junior.tencent.sortandfind;
+
+public class Solution02 {
+    public int search(int[] nums, int target) {
+        int lo = 0, hi = nums.length - 1, mid = 0;
+        while (lo <= hi) {
+            mid = lo + (hi - lo) / 2;
+            if (nums[mid] == target) {
+                return mid;
+            }
+            if (nums[mid] >= nums[lo]) {
+                if (target >= nums[lo] && target < nums[mid]) {
+                    hi = mid - 1;
+                } else {
+                    lo = mid + 1;
+                }
+            } else {
+                if (target > nums[mid] && target <= nums[hi]) {
+                    lo = mid + 1;
+                } else {
+                    hi = mid - 1;
+                }
+            }
+        }
+        return -1;
+    }
+}
