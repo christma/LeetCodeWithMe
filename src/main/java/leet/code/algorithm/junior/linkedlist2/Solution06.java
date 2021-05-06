a280da1abe8234beb192d92a9a99975fe5015bad
christma <liuchuanmumumu@gmail.com>
2021-05-07 07:07:05 +0800

    Solution06

---
+++ src/main/java/leet/code/algorithm/junior/linkedlist2/Solution06.java
@@ -0,0 +1,25 @@
+package leet.code.algorithm.junior.linkedlist2;
+
+public class Solution06 {
+    static class ListNode {
+        int val;
+        ListNode next;
+
+        ListNode(int x) {
+            val = x;
+            next = null;
+        }
+    }
+
+    public boolean hasCycle(ListNode head) {
+        ListNode fast = head, slow = head;
+        while (fast != null && fast.next != null) {
+            fast = fast.next.next;
+            slow = slow.next;
+            if (fast == slow) {
+                return true;
+            }
+        }
+        return false;
+    }
+}
