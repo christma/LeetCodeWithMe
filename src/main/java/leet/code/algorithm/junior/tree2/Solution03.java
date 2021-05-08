80295efe7553321d4267c888a0c5382984f67f56
christma <liuchuanmumumu@gmail.com>
2021-05-09 07:22:28 +0800

    Solution03

---
+++ src/main/java/leet/code/algorithm/junior/tree2/Solution03.java
@@ -0,0 +1,40 @@
+package leet.code.algorithm.junior.tree2;
+
+public class Solution03 {
+    static class TreeNode {
+        int val;
+        TreeNode left;
+        TreeNode right;
+
+        TreeNode() {
+        }
+
+        TreeNode(int val) {
+            this.val = val;
+        }
+
+        TreeNode(int val, TreeNode left, TreeNode right) {
+            this.val = val;
+            this.left = left;
+            this.right = right;
+        }
+    }
+
+    public boolean isSymmetric(TreeNode root) {
+
+        if (root == null) {
+            return true;
+        }
+        return isSymmetric(root.left, root.right);
+    }
+
+    private boolean isSymmetric(TreeNode left, TreeNode right) {
+        if (left == null && right == null) {
+            return true;
+        }
+        if (left == null || right == null || right.val != left.val) {
+            return false;
+        }
+        return isSymmetric(left.left, right.right) && isSymmetric(left.right, right.left);
+    }
+}
