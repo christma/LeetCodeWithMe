b92245ad740cc1352fae801324749f4545561a5d
christma <liuchuanmumumu@gmail.com>
2021-05-15 10:51:10 +0800

    Solution03

---
+++ src/main/java/leet/code/algorithm/junior/dynamic2/Solution.java
@@ -0,0 +1,15 @@
+package leet.code.algorithm.junior.dynamic2;
+
+public class Solution {
+    public int maxSubArray(int[] nums) {
+
+        int length = nums.length;
+        int max = nums[0];
+        int cur = max;
+        for (int i = 1; i < length; i++) {
+            cur = Math.max(cur, 0) + nums[i];
+            max = Math.max(cur, max);
+        }
+        return max;
+    }
+}
