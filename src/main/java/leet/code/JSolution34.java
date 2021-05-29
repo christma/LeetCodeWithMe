1f2c31ec4a09bdd0ba8013f557bfd4659a7ed791
christma <liuchuanmumumu@gmail.com>
2021-05-29 21:57:32 +0800

    JSolution34

---
+++ src/main/java/leet/code/JSolution34.java
@@ -0,0 +1,48 @@
+package leet.code;
+
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.List;
+
+public class JSolution34 {
+    static class TreeNode {
+        int val;
+        TreeNode left;
+        TreeNode right;
+
+        TreeNode() {
+        }
+
+        TreeNode(int val) {
+            this.val = val;
+        }
+
+        TreeNode(int val, TreeNode left, TreeNode right) {
+            this.val = val;
+            this.left = left;
+            this.right = right;
+        }
+    }
+
+    LinkedList<Integer> path = new LinkedList<>();
+    List<List<Integer>> res = new ArrayList<>();
+
+    public List<List<Integer>> pathSum(TreeNode root, int target) {
+        recur(root, target);
+        return res;
+    }
+
+    private void recur(TreeNode root, int target) {
+        if (root == null) {
+            return;
+        }
+        path.add(root.val);
+        target -= root.val;
+        if (target == 0 && root.left == null && root.right == null) {
+            res.add(new LinkedList<>(path));
+        }
+        recur(root.left, target);
+        recur(root.right, target);
+        path.removeLast();
+    }
+}
