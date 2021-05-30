ce987de0a11eb5379910c38ad67921a45e22853b
christma <liuchuanmumumu@gmail.com>
2021-05-31 06:39:43 +0800

    Solution103

---
+++ src/main/java/leet/code/Solution113.java
@@ -0,0 +1,47 @@
+package leet.code;
+
+import java.util.LinkedList;
+import java.util.List;
+
+public class Solution113 {
+    static class TreeNode {
+        int val;
+        TreeNode left;
+        TreeNode right;
+
+        TreeNode() {
+        }
+
+        TreeNode(int val) {
+            this.val = val;
+        }
+
+        TreeNode(int val, TreeNode left, TreeNode right) {
+            this.val = val;
+            this.left = left;
+            this.right = right;
+        }
+    }
+
+    List<List<Integer>> ans = new LinkedList<>();
+    LinkedList<Integer> path = new LinkedList<>();
+
+    public List<List<Integer>> pathSum(TreeNode root, int targetSum) {
+        dfs(root, targetSum);
+        return ans;
+    }
+
+    private void dfs(TreeNode root, int targetSum) {
+        if (root == null) {
+            return;
+        }
+        path.add(root.val);
+        targetSum -= root.val;
+        if (targetSum == 0 && root.left == null && root.right == null) {
+            ans.add(new LinkedList<>(path));
+        }
+        dfs(root.left, targetSum);
+        dfs(root.right, targetSum);
+        path.removeLast();
+    }
+}
