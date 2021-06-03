077c0bd6bf99ab16715e9a7d5fdd9064b1f7c506
christma <liuchuanmumumu@gmail.com>
2021-06-04 07:19:17 +0800

    Solution515

---
+++ src/main/java/leet/code/Solution515.java
@@ -0,0 +1,53 @@
+package leet.code;
+
+import java.util.ArrayList;
+import java.util.Deque;
+import java.util.LinkedList;
+import java.util.List;
+
+public class Solution515 {
+    class TreeNode {
+        int val;
+        TreeNode left;
+        TreeNode right;
+
+        TreeNode() {
+        }
+
+        TreeNode(int val) {
+            this.val = val;
+        }
+
+        TreeNode(int val, TreeNode left, TreeNode right) {
+            this.val = val;
+            this.left = left;
+            this.right = right;
+        }
+    }
+
+    public List<Integer> largestValues(TreeNode root) {
+        if (root == null) return new ArrayList<>();
+
+        List<Integer> ans = new LinkedList<>();
+        Deque<TreeNode> q = new LinkedList<>();
+        int max = Integer.MIN_VALUE;
+        q.add(root);
+        while (!q.isEmpty()) {
+            int size = q.size();
+
+            for (int i = 0; i < size; i++) {
+                TreeNode temp = q.poll();
+                max = Math.max(max, temp.val);
+                if (temp.left != null) {
+                    q.add(temp.left);
+                }
+                if (temp.right != null) {
+                    q.add(temp.right);
+                }
+            }
+            ans.add(max);
+            max = Integer.MIN_VALUE;
+        }
+        return ans;
+    }
+}
