db7daead7bea7db1d7358c55fc66722197f469e1
christma <liuchuanmumumu@gmail.com>
2021-06-07 07:06:55 +0800

    Solution958

---
+++ src/main/java/leet/code/Solution958.java
@@ -0,0 +1,45 @@
+package leet.code;
+
+import java.util.Deque;
+import java.util.LinkedList;
+
+public class Solution958 {
+    static class TreeNode {
+        int val;
+        TreeNode left;
+        TreeNode right;
+
+        TreeNode() {
+        }
+
+        TreeNode(int val) {
+            this.val = val;
+        }
+
+        TreeNode(int val, TreeNode left, TreeNode right) {
+            this.val = val;
+            this.left = left;
+            this.right = right;
+        }
+    }
+
+    public boolean isCompleteTree(TreeNode root) {
+
+        Deque<TreeNode> deque = new LinkedList<>();
+        deque.offer(root);
+        boolean reachedEnd = false;
+        while (!deque.isEmpty()) {
+            TreeNode node = deque.poll();
+            if (reachedEnd && node != null) {
+                return false;
+            }
+            if (node == null) {
+                reachedEnd = true;
+                continue;
+            }
+            deque.offer(node.left);
+            deque.offer(node.right);
+        }
+        return true;
+    }
+}
