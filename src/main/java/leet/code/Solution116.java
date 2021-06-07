52767297b9591502bb687fec6595e8a79876dae0
christma <liuchuanmumumu@gmail.com>
2021-06-08 06:57:15 +0800

    Solution116

---
+++ src/main/java/leet/code/Solution116.java
@@ -0,0 +1,52 @@
+package leet.code;
+
+import java.util.Deque;
+import java.util.LinkedList;
+
+public class Solution116 {
+    static class Node {
+        public int val;
+        public Node left;
+        public Node right;
+        public Node next;
+
+        public Node() {
+        }
+
+        public Node(int _val) {
+            val = _val;
+        }
+
+        public Node(int _val, Node _left, Node _right, Node _next) {
+            val = _val;
+            left = _left;
+            right = _right;
+            next = _next;
+        }
+    }
+
+    public Node connect(Node root) {
+
+        if (root == null) {
+            return null;
+        }
+        Deque<Node> deque = new LinkedList<>();
+        deque.offer(root);
+        while (!deque.isEmpty()) {
+            int size = deque.size();
+            for (int i = 0; i < size; i++) {
+                Node node = deque.poll();
+                if (i < size - 1) {
+                    node.next = deque.peek();
+                }
+                if (node.left != null) {
+                    deque.offer(node.left);
+                }
+                if (node.right != null) {
+                    deque.offer(node.right);
+                }
+            }
+        }
+        return root;
+    }
+}
