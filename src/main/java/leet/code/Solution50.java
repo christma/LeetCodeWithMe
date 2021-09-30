0d1d405080078da112c9fa09a17c623ce8cb2b28
christma <liuchuanmumumu@gmail.com>
2021-09-30 13:40:14 +0800

    company commit

--- src/main/java/leet/code/Solution50.java
+++ src/main/java/leet/code/Solution50.java
@@ -3,7 +3,7 @@ package leet.code;
 public class Solution50 {
     public double myPow(double x, int n) {
         long N = n;
-        return N >= 0 ? quickMul(x, N) : quickMul(x, 1 / N);
+        return N >= 0 ? quickMul(x, N) : 1/quickMul(x, -N);
     }
 
     private double quickMul(double x, long n) {
