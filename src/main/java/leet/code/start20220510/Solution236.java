f8d8c320811741af1bd30d0021c80e08b6b2d40f
christma <liuchuanmumumu@gmail.com>
2022-05-18 20:09:52 +0800

    Solution236

---
+++ src/main/java/leet/code/start20220510/Solution236.java
@@ -0,0 +1,19 @@
+package leet.code.start20220510;
+
+public class Solution236 {
+
+    public TreeNode lowestCommonAncestor(TreeNode root, TreeNode p, TreeNode q) {
+
+        if (root == null || root == p || root == q) {
+            return root;
+        }
+        TreeNode left = lowestCommonAncestor(root.left, p, q);
+        TreeNode right = lowestCommonAncestor(root.right, p, q);
+        if (left == null && right == null) return null;
+        if (left == null) return right;
+        if (right == null) return left;
+        return root;
+
+    }
+
+}
