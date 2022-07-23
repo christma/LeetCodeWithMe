3e1917ecc959a6e5183a3d22875ddab7e9143562
christma <liuchuanmumumu@gmail.com>
2022-07-23 16:48:55 +0800

    Solution56

---
+++ src/main/java/leet/code/start20220510/Solution56.java
@@ -0,0 +1,30 @@
+package leet.code.start20220510;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Comparator;
+
+public class Solution56 {
+    public int[][] merge(int[][] intervals) {
+
+        if (intervals.length == 0)
+            return new int[][]{};
+        Arrays.sort(intervals, new Comparator<int[]>() {
+            @Override
+            public int compare(int[] o1, int[] o2) {
+                return o1[0] - o2[0];
+            }
+        });
+
+        ArrayList<int[]> merged = new ArrayList<>();
+        for (int i = 0; i < intervals.length; i++) {
+            int L = intervals[i][0], R = intervals[i][1];
+            if (merged.size() == 0 || merged.get(merged.size() - 1)[1] < L) {
+                merged.add(new int[]{L, R});
+            } else {
+                merged.get(merged.size() - 1)[1] = Math.max(merged.get(merged.size() - 1)[1], R);
+            }
+        }
+        return merged.toArray(new int[merged.size()][]);
+    }
+}
