6f05a34176f20828feb0c644049b2d757f0b6fd2
christma <liuchuanmumumu@gmail.com>
2022-09-01 09:50:49 +0800

    Solution27

---
+++ src/main/java/leet/code/start20220510/Solution27.java
@@ -0,0 +1,16 @@
+package leet.code.start20220510;
+
+public class Solution27 {
+    public int removeElement(int[] nums, int val) {
+        int n = nums.length;
+        int left = 0;
+        for (int right = 0; right < n; right++) {
+            if (nums[right] != val) {
+                nums[left] = nums[right];
+                left++;
+            }
+        }
+        return left;
+
+    }
+}
