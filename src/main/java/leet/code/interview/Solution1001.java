5e08d15f43ee0d79316a9dde4960fda0b67a2dc4
christma <liuchuanmumumu@gmail.com>
2022-11-25 15:29:35 +0800

    Solution1001

---
+++ src/main/java/leet/code/interview/Solution1001.java
@@ -0,0 +1,24 @@
+package leet.code.interview;
+
+public class Solution1001 {
+    public void merge(int[] A, int m, int[] B, int n) {
+        int pa = 0, pb = 0;
+        int[] sorted = new int[m + n];
+        int cur;
+        while (pa < m || pb < n) {
+            if (pa == m) {
+                cur = B[pb++];
+            } else if (pb == n) {
+                cur = A[pa++];
+            } else if (A[pa] < B[pb]) {
+                cur = A[pa++];
+            } else {
+                cur = B[pb++];
+            }
+            sorted[pa + pb - 1] = cur;
+        }
+        for (int i = 0; i < m + n; i++) {
+            A[i] = sorted[i];
+        }
+    }
+}
