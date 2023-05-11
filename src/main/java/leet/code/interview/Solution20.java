d3ea9babb54aa637b1d882a9ceba8189c7f80acd
chirstma <liuchuanmumumu@gmail.com>
2023-05-11 20:59:52 +0800

    update

---
+++ src/main/java/leet/code/interview/Solution20.java
@@ -0,0 +1,36 @@
+package leet.code.interview;
+
+import java.util.*;
+
+public class Solution20 {
+    public boolean isValid(String s) {
+        int n = s.length();
+        if (n % 2 == 1) {
+            return false;
+        }
+
+        Map<Character, Character> pairs = new HashMap<Character, Character>() {{
+            put(')', '(');
+            put(']', '[');
+            put('}', '{');
+        }};
+        Deque<Character> stack = new LinkedList<Character>();
+        for (int i = 0; i < n; i++) {
+            char ch = s.charAt(i);
+            if (pairs.containsKey(ch)) {
+                if (stack.isEmpty() || stack.peek() != pairs.get(ch)) {
+                    return false;
+                }
+                stack.pop();
+            } else {
+                stack.push(ch);
+            }
+        }
+        return stack.isEmpty();
+
+    }
+
+    public static void main(String[] args) {
+        System.out.println(new Solution20().isValid("([)]"));
+    }
+}
