5905131d3bf0655d50382ebb3ba144dd0f1a1c73
chirstma <liuchuanmumumu@gmail.com>
2023-05-18 20:58:09 +0800

    update

--- src/main/java/leet/code/tesla/Solution1304.java
+++ src/main/java/leet/code/tesla/Solution1304.java
@@ -1,19 +1,24 @@
 package leet.code.tesla;
 
+import java.util.Arrays;
+
 public class Solution1304 {
     public int[] sumZero(int n) {
-
         int[] ans = new int[n];
-        int idx = 0;
-        int tmp = 1;
+        int flag = 0;
+        int temp = 1;
         if (n % 2 != 0) {
-            ans[idx++] = 0;
+            ans[flag++] = 0;
         }
-        while (idx < n) {
-            ans[idx++] = tmp;
-            ans[idx++] = -tmp;
-            tmp++;
+        while (flag < n) {
+            ans[flag++] = temp;
+            ans[flag++] = -temp;
+            temp++;
         }
         return ans;
+    }
+
+    public static void main(String[] args) {
+        System.out.println(Arrays.stream(new Solution1304().sumZero(7)).iterator().next());
     }
 }
