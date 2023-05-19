c4a870f761cf8c7ac0dce1cb8481c89669461f4f
chirstma <liuchuanmumumu@gmail.com>
2023-05-19 21:47:38 +0800

    update

--- src/main/java/leet/code/tesla/Solution145.java
+++ src/main/java/leet/code/tesla/Solution145.java
@@ -1,6 +1,8 @@
 package leet.code.tesla;
 
 import java.util.ArrayList;
+import java.util.Deque;
+import java.util.LinkedList;
 import java.util.List;
 
 public class Solution145 {
@@ -14,11 +16,38 @@ public class Solution145 {
     }
 
     private void postOrder(TreeNode root, ArrayList<Integer> ans) {
-        if (ans == null) {
+        if (root == null) {
             return;
         }
         postOrder(root.left, ans);
         postOrder(root.right, ans);
         ans.add(root.val);
     }
+
+
+    public List<Integer> postorderTraversal2(TreeNode root) {
+        ArrayList<Integer> res = new ArrayList<>();
+        if (root == null) return res;
+
+        Deque<TreeNode> stack = new LinkedList<>();
+        TreeNode prev = null;
+        while (root != null || !stack.isEmpty()) {
+            while (root != null) {
+                stack.push(root);
+                root = root.left;
+            }
+            root = stack.pop();
+            if (root.right == null || root.right == prev) {
+                res.add(root.val);
+                prev = root;
+                root = null;
+            } else {
+                stack.push(root);
+                root = root.right;
+            }
+
+        }
+        return res;
+    }
+
 }
