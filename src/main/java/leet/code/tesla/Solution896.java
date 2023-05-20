5958d7cb7f5175bb7df1e8f62eee55626c0775c9
chirstma <liuchuanmumumu@gmail.com>
2023-05-20 23:20:45 +0800

    update

---
+++ src/main/java/leet/code/tesla/Solution896.java
@@ -0,0 +1,17 @@
+package leet.code.tesla;
+
+public class Solution896 {
+    public boolean isMonotonic(int[] nums) {
+        boolean inc = true, dec = true;
+        int n = nums.length;
+        for (int i = 0; i < n - 1; i++) {
+            if (nums[i] > nums[i + 1]) {
+                inc = false;
+            }
+            if (nums[i] < nums[i + 1]) {
+                dec = false;
+            }
+        }
+        return inc || dec;
+    }
+}
