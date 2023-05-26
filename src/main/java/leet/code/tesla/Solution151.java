fb7bd51a172274c62aa95451c3f5d7400948ec6a
chirstma <liuchuanmumumu@gmail.com>
2023-05-26 22:49:44 +0800

    update

---
+++ src/main/java/leet/code/tesla/Solution151.java
@@ -0,0 +1,22 @@
+package leet.code.tesla;
+
+public class Solution151 {
+    public String reverseWords(String s) {
+
+        String trim = s.trim();
+        String[] split = trim.split(" ");
+        StringBuilder sb = new StringBuilder();
+        for (int i = split.length-1; i >= 0; i--) {
+            if(!split[i].isEmpty()){
+                sb.append(split[i]);
+                sb.append(" ");
+            }
+
+        }
+        return sb.toString().trim();
+    }
+
+    public static void main(String[] args) {
+        System.out.println(new Solution151().reverseWords("blue is sky the "));
+    }
+}
