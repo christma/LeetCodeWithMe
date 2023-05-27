c1f78f8388cb97668837f7605bd6de1dc2b97748
chirstma <liuchuanmumumu@gmail.com>
2023-05-27 23:24:17 +0800

    update

---
+++ src/main/java/leet/code/tesla/Solution1.java
@@ -0,0 +1,16 @@
+package leet.code.tesla;
+
+import java.util.HashMap;
+
+public class Solution1 {
+    public int[] twoSum(int[] nums, int target) {
+        HashMap<Integer, Integer> map = new HashMap<>();
+        for (int i = 0; i < nums.length; i++) {
+            if (map.containsKey(target - nums[i])) {
+                return new int[]{map.get(target - nums[i]), i};
+            }
+            map.put(nums[i], i);
+        }
+        return new int[0];
+    }
+}
