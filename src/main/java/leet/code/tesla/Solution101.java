079c23692a526e2cc04fb49b3a8f239c91c4dbc5
chirstma <liuchuanmumumu@gmail.com>
2023-06-01 10:14:22 +0800

    update

---
+++ src/main/java/leet/code/tesla/Solution101.java
@@ -0,0 +1,21 @@
+package leet.code.tesla;
+
+public class Solution101 {
+    public boolean isSymmetric(TreeNode root) {
+
+        return check(root, root);
+
+    }
+
+    private boolean check(TreeNode r1, TreeNode r2) {
+        if (r1 == null && r2 == null) {
+            return true;
+        }
+
+        if (r1 == null || r2 == null) {
+            return false;
+        }
+
+        return r1.val == r2.val && check(r1.left, r2.right) && check(r1.right, r2.left);
+    }
+}
