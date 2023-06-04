6c5fa8570642f6a26a893c32e9b6297c429372a3
chirstma <liuchuanmumumu@gmail.com>
2023-06-04 23:04:17 +0800

    update

---
+++ src/main/java/leet/code/tree/Solution104.java
@@ -0,0 +1,14 @@
+package leet.code.tree;
+
+public class Solution104 {
+    public int maxDepth(TreeNode root) {
+
+        if (root == null) {
+            return 0;
+        } else {
+            int leftHeight = maxDepth(root.left);
+            int rightHeight = maxDepth(root.right);
+            return Math.max(leftHeight, rightHeight) + 1;
+        }
+    }
+}
