3c45d178f3abc444341c19c58c2924010d45e51b
chirstma <liuchuanmumumu@gmail.com>
2023-06-05 21:46:51 +0800

    update

--- src/main/java/leet/code/tree/Solution104.java
+++ src/main/java/leet/code/tree/Solution104.java
@@ -1,7 +1,10 @@
 package leet.code.tree;
 
+import java.util.Deque;
+import java.util.LinkedList;
+
 public class Solution104 {
-    public int maxDepth(TreeNode root) {
+    public int maxDepth1(TreeNode root) {
 
         if (root == null) {
             return 0;
@@ -10,5 +13,33 @@ public class Solution104 {
             int rightHeight = maxDepth(root.right);
             return Math.max(leftHeight, rightHeight) + 1;
         }
+    }
+
+
+    public int maxDepth(TreeNode root) {
+
+        if (root == null) {
+            return 0;
+        }
+
+        Deque<TreeNode> deque = new LinkedList<>();
+        deque.offer(root);
+        int ans = 0;
+        while (!deque.isEmpty()) {
+            int size = deque.size();
+            while (size > 0) {
+                TreeNode node = deque.poll();
+                if (node.left != null) {
+                    deque.offer(node.left);
+                }
+                if (node.right != null) {
+                    deque.offer(node.right);
+                }
+                size--;
+            }
+            ans++;
+        }
+        return ans;
+
     }
 }
