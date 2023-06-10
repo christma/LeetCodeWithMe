be129d7c692cea4832e9cac274135e25490662e2
christma <liuchuanmumumu@gmail.com>
2023-06-10 22:18:05 +0800

    update

---
+++ src/main/java/leet/code/tree/Solution112.java
@@ -0,0 +1,15 @@
+package leet.code.tree;
+
+public class Solution112 {
+    public boolean hasPathSum(TreeNode root, int targetSum) {
+
+        if (root == null) {
+            return false;
+        }
+        if (root.left == null && root.right == null) {
+            return targetSum == root.val;
+        }
+        return hasPathSum(root.left, targetSum - root.val)
+                || hasPathSum(root.right, targetSum - root.val);
+    }
+}
