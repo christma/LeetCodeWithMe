e6cae58c3cd169625d1465943ce140e486a6fe95
christma <liuchuanmumumu@gmail.com>
2023-06-19 21:26:36 +0800

    update

---
+++ src/main/java/leet/code/tree/Solution110.java
@@ -0,0 +1,24 @@
+package leet.code.tree;
+
+public class Solution110 {
+    public boolean isBalanced(TreeNode root) {
+
+        return height(root) >= 0;
+    }
+
+    private int height(TreeNode root) {
+        if (root == null) {
+            return 0;
+        }
+
+        int leftHeight = height(root.left);
+        int rightHeight = height(root.right);
+        if (leftHeight == -1 || rightHeight == -1 || Math.abs(leftHeight - rightHeight) > 1) {
+            return -1;
+        } else {
+            return Math.max(leftHeight, rightHeight) + 1;
+        }
+
+
+    }
+}
--- src/main/java/leet/code/tree/Solution111.java
+++ src/main/java/leet/code/tree/Solution111.java
@@ -11,4 +11,19 @@ public class Solution111 {
         int right = minDepth(root.right);
         return root.left == null || root.right == null ? left + right + 1 : Math.min(left, right) + 1;
     }
+
+
+    public static void main(String[] args) {
+        TreeNode t0 = new TreeNode(3);
+        TreeNode t1 = new TreeNode(9);
+        TreeNode t2 = new TreeNode(20);
+        TreeNode t3 = new TreeNode(15);
+        TreeNode t4 = new TreeNode(7);
+
+        t0.left = t1;
+        t0.right = t2;
+        t2.left = t3;
+        t3.right = t4;
+        new Solution111().minDepth(t0);
+    }
 }
