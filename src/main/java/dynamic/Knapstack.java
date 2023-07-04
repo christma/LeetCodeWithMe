e0fdd9722f78951ae382382b8bf13b33b0ea3459
christma <liuchuanmumumu@gmail.com>
2023-07-04 10:19:36 +0800

    update

--- src/main/java/dynamic/Knapstack.java
+++ src/main/java/dynamic/Knapstack.java
@@ -22,10 +22,29 @@ public class Knapstack {
         return Math.max(p1, p2);
     }
 
+    private static int dp(int[] w, int[] v, int bag) {
+        int N = w.length;
+        int[][] dp = new int[N + 1][bag + 1];
+        for (int index = N - 1; index >= 0; index--) {
+            for (int rest = 0; rest <= bag; rest++) {
+                int p1 = dp[index + 1][rest];
+                int p2 = 0;
+                int next = rest - w[index] < 0 ? -1 : dp[index + 1][rest - w[index]];
+                if (next != -1) {
+                    p2 = v[index] + next;
+                }
+                dp[index][rest] = Math.max(p1, p2);
+            }
+        }
+        return dp[0][bag];
+    }
+
+
     public static void main(String[] args) {
         int[] w = {3, 2, 4, 7, 3, 1, 7};
         int[] v = {5, 6, 3, 19, 12, 4, 2};
         int bag = 15;
         System.out.println(maxValue(w, v, bag));
+        System.out.println(dp(w, v, bag));
     }
 }
