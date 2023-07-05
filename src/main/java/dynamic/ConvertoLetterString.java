ace2cc6dc77e18543f8bdda55c1d9aa11d38d35a
christma <liuchuanmumumu@gmail.com>
2023-07-05 14:43:47 +0800

    update

--- src/main/java/dynamic/ConvertoLetterString.java
+++ src/main/java/dynamic/ConvertoLetterString.java
@@ -22,8 +22,29 @@ public class ConvertoLetterString {
         return ans;
     }
 
+    public static int dp(String s) {
+        if (s == null && s.length() == 0) {
+            return 0;
+        }
+        char[] str = s.toCharArray();
+        int N = str.length;
+        int[] dp = new int[N + 1];
+        dp[N] = 1;
+        for (int i = N - 1; i >= 0; i--) {
+            if (str[i] != 0) {
+                int ways = dp[i + 1];
+                if (i + 1 < str.length && (str[i] - '0') * 10 + (str[i + 1] - '0') < 27) {
+                    ways += dp[i + 2];
+                }
+                dp[i] = ways;
+            }
+
+        }
+        return dp[0];
+    }
 
     public static void main(String[] args) {
         System.out.println(number("111"));
+        System.out.println(dp("111"));
     }
 }
