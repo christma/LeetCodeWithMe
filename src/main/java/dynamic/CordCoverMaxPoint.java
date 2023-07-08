38a646e9bddff683c5fba211c8b9a361e626b781
christma <liuchuanmumumu@gmail.com>
2023-07-08 15:31:19 +0800

    update

---
+++ src/main/java/dynamic/CordCoverMaxPoint.java
@@ -0,0 +1,47 @@
+package dynamic;
+
+public class CordCoverMaxPoint {
+    public static int maxPoint1(int[] arr, int L) {
+        int res = 1;
+        for (int i = 0; i < arr.length; i++) {
+            int nearest = nearestIndex(arr, i, arr[i] - L);
+            res = Math.max(res, i - nearest + 1);
+
+        }
+        return res;
+    }
+
+    private static int nearestIndex(int[] arr, int R, int value) {
+
+        int L = 0;
+        int index = R;
+        while (L <= R) {
+            int mid = L + ((R - L) >> 1);
+            if (arr[mid] >= value) {
+                index = mid;
+                R = mid - 1;
+            } else {
+                L = mid + 1;
+            }
+        }
+        return index;
+    }
+
+
+    public static int maxPoint2(int[] arr, int L) {
+        int left = 0;
+        int right = 0;
+        int N = arr.length;
+        int max = 0;
+        while (left < N) {
+            while (right < N && arr[right] - arr[left] <= L) {
+                right++;
+            }
+            max = Math.max(max, right - left);
+            left++;
+        }
+        return max;
+    }
+
+
+}
